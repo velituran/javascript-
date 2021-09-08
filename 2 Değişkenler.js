6a067188e3ac2975786640746013ff3ae437a22f
velituran <90135312+velituran@users.noreply.github.com>
2021-09-09 02:55:07 +0300

    Add files via upload

---
+++ 2 Değişkenler.js
@@ -0,0 +1,51 @@
+//#Ders 2 DEĞİŞKENLER
+
+var age;
+console.log(age);
+
+age=20;
+ console.log(age);
+
+var fullname='Sadık Turan';
+ console.log(fullname);
+
+var fullname='Çınar Turan';
+console.log(fullname);
+
+// DEĞİŞKEN TANIMLAMA KURALLARI
+
+//sayısal ifade ile başlayamaz
+
+var yas1;
+var _yas2;
+var $yas3;
+
+//komut isimleriyle tanımlama yapılamaz
+
+//Birden fazla kelime
+
+var ad='Veli';
+var soyad="TUran";
+
+
+var ad_soyad='Veli Turan';
+var adSoyad="Ahmet Turan";
+
+
+ // case senstive (büyük kücük harf duyarlılığı)
+
+var firstName="Veli";
+var FirstName="Ahmet";
+
+console.log(firstName);
+console.log(FirstName);
+
+// var, let , const
+
+let city="Ankara";
+console.log(city);
+
+const email="abc@gmail.com";
+console.log(email);
+
+email ="bcd@gmail.com";
