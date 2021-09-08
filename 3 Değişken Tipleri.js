fe585e3dc01af6e80309e44ac99ec38b112e525a
velituran <90135312+velituran@users.noreply.github.com>
2021-09-09 02:55:47 +0300

    Add files via upload

---
+++ 3 Değişken Tipleri.js
@@ -0,0 +1,46 @@
+//#Ders 3 DEĞİŞKEN TİPLERİ
+
+//PRİMİTİVE TYPES
+
+//-String
+
+let firstName="Veli";
+
+ //-Number
+
+let age=20;
+let money=100.5;
+
+ //-Boolean
+
+let isActive=false
+
+ //-null
+let job=null;
+
+//-undefined
+let car;
+
+
+// //REFERANCE TYPES-OBJECT
+
+//-Array
+
+ let names =(["Ali","Ahmet","Can"]);
+
+//-Object
+
+ let address={
+   city:"Ankara",
+   country:"Türkiye"
+
+    
+}
+
+//-Function
+
+var calcuateAge=function(){
+  return 0;
+
+    
+ };
